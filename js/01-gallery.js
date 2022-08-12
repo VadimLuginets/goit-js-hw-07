7b69a5d1d8125e72c2f0a853b5b1d0c4258a8a3e
Vadim luginets <pebeeo@gmail.com>
2022-08-12 20:04:33 +0300

    task 1, 2
    works

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const genGalleryGrid = galleryItems
+  .map((item) => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </div>`;
+  })
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", genGalleryGrid);
+console.log(genGalleryGrid);
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const showOriginalImg = basicLightbox.create(
+    `<img src="${
+      e.target.closest("img").dataset.source
+    }" width="800" height="600">`
+  );
+  console.log(showOriginalImg.show());
+});
+document.addEventListener("keydown", (e) => {
+  const escape = e.code === "Escape";
+  const modal = document.querySelector(".basicLightbox");
+  if (!modal) {
+    return;
+  }
+  if (escape) {
+    modal.remove();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const genGalleryGrid = galleryItems
+  .map((item) => {
+    return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+</a>`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", genGalleryGrid);
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  let gallery = new SimpleLightbox(".gallery a");
+
+  gallery.on("show.simplelightbox", function () {
+    gallery.defaultOptions.captionDelay = 250;
+  });
+});
